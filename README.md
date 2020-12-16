3c16de4119dd27fc2f18bfb20577f7a50d22e886
Jean-Francois Allaire <jfallaire@coveo.com>
2020-12-16 11:46:34 -0500

    Adds hosted search page web component

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# hosted-search-page
+# HostedSearchPage
+
+Disclaimer: This component was built by the community at large and is not an official Coveo JSUI Component. Use this component at your own risk.
+
+## Getting Started
+
+1. Install the component into your project.
+
+```
+npm i @coveops/hosted-search-page
+```
+
+2. Use the Component or extend it
+
+Typescript:
+
+```javascript
+import { HostedSearchPage, IHostedSearchPageOptions } from '@coveops/hosted-search-page';
+```
+
+Javascript
+
+```javascript
+const HostedSearchPage = require('@coveops/hosted-search-page').HostedSearchPage;
+```
+
+3. You can also expose the component alongside other components being built in your project.
+
+```javascript
+export * from '@coveops/hosted-search-page'
+```
+
+4. Or for quick testing, you can add the script from unpkg
+
+```html
+<script src="https://unpkg.com/@coveops/hosted-search-page@latest/dist/index.min.js"></script>
+```
+
+> Disclaimer: Unpkg should be used for testing but not for production.
+
+5. Include the component in your template as follows:
+
+Place the component in your markup:
+
+```html
+<div class="CoveoHostedSearchPage"></div>
+```
+
+## Extending
+
+Extending the component can be done as follows:
+
+```javascript
+import { HostedSearchPage, IHostedSearchPageOptions } from "@coveops/hosted-search-page";
+
+export interface IExtendedHostedSearchPageOptions extends IHostedSearchPageOptions {}
+
+export class ExtendedHostedSearchPage extends HostedSearchPage {}
+```
+
+## Contribute
+
+1. Clone the project
+2. Copy `.env.dist` to `.env` and update the COVEO_ORG_ID and COVEO_TOKEN fields in the `.env` file to use your Coveo credentials and SERVER_PORT to configure the port of the sandbox - it will use 8080 by default.
+3. Build the code base: `npm run build`
+4. Serve the sandbox for live development `npm run serve`
---
+++ src/HostedSearchPage.ts
@@ -0,0 +1,194 @@
+
+const template = document.createElement('template');
+template.innerHTML = `<div id="hostedSearchPage"></div>`;
+
+interface IHostedSearchPageAsset {
+  InlineContent: string,
+  Name: string,
+  URL: string
+}
+
+interface IHostedSearchPage {
+  id: string,
+  name: string,
+  title: string,
+  lastModified: string,
+  html: string,
+  css: Array<IHostedSearchPageAsset>,
+  javascript: Array<IHostedSearchPageAsset>
+}
+
+export interface IHostedSearchPageOptions {
+  orgId: string,
+  pageId: string,
+  apiKey: string,
+  htmlOnly?: boolean
+}
+
+export interface IHostedSearchPageState {
+  scriptsLoaded: number,
+  error: string
+}
+
+export class HostedSearchPage extends HTMLElement {
+  private _state: IHostedSearchPageState;
+  private _options: IHostedSearchPageOptions = {
+    orgId:'',
+    pageId: '',
+    apiKey: '',
+    htmlOnly: false
+  };
+  private _searchPage: HTMLElement;
+  private _searchPageScripts: Array<IHostedSearchPageAsset>;
+  private _searchPageStyles: Array<IHostedSearchPageAsset>;
+  private _root: HTMLElement|ShadowRoot;
+  private _attachShadow: boolean;
+
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this._attachShadow = this.getAttribute('attachShadow') && this.getAttribute('attachShadow') == 'true';
+    if(!this._attachShadow) {
+      this._root = this;
+    } else {
+      this.attachShadow({mode: 'open'});
+      this._root = this.shadowRoot; 
+    }
+
+    this._root.appendChild(template.content.cloneNode(true));
+    this.searchPage = this._root.querySelector('#hostedSearchPage');
+    this.state = {
+      scriptsLoaded: 0,
+      error: ''
+    }
+  }
+
+  getModel() {
+    const {orgId, pageId, apiKey } = this._options;
+    return new Promise((res, rej) => {
+      fetch(`https://platform.cloud.coveo.com/rest/organizations/${orgId}/pages/${pageId}`, {
+        headers: new Headers({
+          'Authorization': `Bearer ${apiKey}`
+        })
+      })
+        .then(data => data.json())
+        .then((json) => {
+          this.renderSearchPage(json);
+          res();
+        })
+        .catch((error) => rej(error));
+    })
+  }
+
+  public configure(options: IHostedSearchPageOptions){
+    this.options = options;
+    this.getModel();
+  }
+
+  private renderSearchPage(data: IHostedSearchPage){
+    console.log(data);
+    this.searchPage.innerHTML = data.html;
+    if(!this.options.htmlOnly){
+      this.addScriptFiles(data);
+      this.addStyleFiles(data);
+    }
+  }
+
+  private addScriptFiles(data: IHostedSearchPage) {
+    if (data.javascript) {
+      this._searchPageScripts = data.javascript.filter(script => script.Name.indexOf('InterfaceEditor') < 0);
+      this._searchPageScripts.forEach(script => {
+        const element = document.createElement('script');
+        element.type = 'text/javascript';
+        if (script.URL){
+          element.src = script.URL;
+          element.async = false;
+          if(script.Name === 'CoveoSearchUI') {
+            element.classList.add('coveo-script')
+          }
+          element.addEventListener('load', () => {
+              this.state = {
+                  scriptsLoaded: this.state.scriptsLoaded + 1,
+                  error: this.state.error
+              };
+              this.coveoScriptsLoaded();
+          }, { once: true });
+          this._attachShadow ? this._root.insertBefore(element, this._searchPage) : document.head.appendChild(element);
+        } else {
+          document.addEventListener('CoveoScriptsLoaded', () => {
+            element.insertAdjacentHTML('beforeend', script.InlineContent);
+            this._attachShadow ? this._root.insertBefore(element, this._searchPage) : document.head.appendChild(element);
+          })
+        }
+      });
+    }
+  }
+
+  private addStyleFiles(data: IHostedSearchPage) {
+    if (data.css) {
+      this._searchPageStyles = data.css.filter(css => css.Name.indexOf('InterfaceEditor') < 0);
+      this._searchPageStyles.forEach(css => {
+        if (css.URL){
+          const element = document.createElement('link');
+          element.href = css.URL;
+          element.rel="stylesheet";
+          element.id = css.Name;
+          this._attachShadow ? this._root.insertBefore(element, this._searchPage) : document.head.appendChild(element);
+          
+        } else {
+          const inlineElement = document.createElement('style');
+          inlineElement.insertAdjacentHTML('beforeend', css.InlineContent);
+          this._attachShadow ? this._root.insertBefore(inlineElement, this._searchPage) : document.head.appendChild(inlineElement);
+        }
+      });
+    }
+  }
+
+  private coveoScriptsLoaded() {
+      if (this.state.scriptsLoaded === this._searchPageScripts.filter(s => s.URL).length) {
+        document.dispatchEvent(new CustomEvent("CoveoScriptsLoaded"));
+      }
+  }
+  
+  // static get observedAttributes() {
+  //   // Indicate that we want to be notified
+  //   // when the `options` attribute is changed
+  //   return ['enableShadow'];
+  // }
+
+  // attributeChangedCallback(attrName, oldVal, newVal) {
+  //   if (oldVal != newVal) {
+  //     // If the value for the `groupName` attribute has changed
+  //     // then insert the value into the `<label>`
+  //     this.options.enableShadow = newVal || true;
+  //     // OR: this._span.innerHTML = newVal || 'no value';
+  //     // But make sure someone has not tried to hit you
+  //     // with a script attack.
+  //   }
+  // }
+
+  get options() {
+    return this._options;
+  }
+  set options(newOptions) {
+    this._options = Object.assign(this._options || {}, newOptions);
+  }
+
+  get state() {
+    return this._state;
+  }
+  set state(newState) {
+    this._state = newState;
+  }
+
+  get searchPage() {
+    return this._searchPage;
+  }
+  set searchPage(newSearchPage) {
+    this._searchPage = newSearchPage;
+  }
+}
+
+customElements.define('hosted-search-page', HostedSearchPage);
